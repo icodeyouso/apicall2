6f60781a9f31d187d4d1600bb021fa6fe2d20c7d
marquis parks <quis90@live.com>
2018-04-12 22:33:40 -0700

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-// var https = require("https");
 
-// const url =
-//   "https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries/5a2e8810758c6e4347e5ece1?$format=json"
-
-
-// https.get(url, res => {
-//   res.setEncoding("utf8");
-//   let body = "";
-//   res.on("data", data => {
-//     body += data;
-//   });
-//   res.on("end", () => {
-//     body = JSON.parse(body);
-//     res.render("index.ejs", {data: body.DisasterDeclarationsSummaries })
-//     console.log(
-//       `state: ${body.DisasterDeclarationsSummaries.state} -`
-
-//     );
-//   });
-// });
 
 
 var express = require('express')
